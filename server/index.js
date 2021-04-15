c2c80e5a684d238af27acf6bd4c2b30348044b27
Carlos Azaustre <cazaustre@gmail.com>
2021-04-15 23:04:59 +0200

    basic read API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 
 const http = require("http");
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/phone-catalog";
@@ -9,4 +11,32 @@ const DB_URI = process.env.DB_URI || "mongodb://localhost:27017/phone-catalog";
 const app = express();
 const server = http.createServer(app);
 
-server.listen(PORT, console.log(`Server running on http://localhost:${PORT}`));
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/", (req, res) => {
+  res.json({ status: "running", date: Date.now() });
+});
+app.get("/api/phones", (req, res) => {
+  res.json({ phones: [] });
+});
+app.get("/api/phone/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ phone: id });
+});
+
+(async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("💽 DB is running and the connection is established");
+
+    await server.listen(PORT);
+    console.log(`🚀 API Server running on http://localhost:${PORT}`);
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
